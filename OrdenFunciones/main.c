bdd2abf4d013a047c9e5ae5303eda3aa6ca80ffc
Leoweb23 <leonardo.mule@docente.ceibal.edu.uy>
2023-04-13 14:53:28 -0300

    Crear la función calcularPromedio Fixes #4

--- OrdenFunciones/main.c
+++ OrdenFunciones/main.c
@@ -4,3 +4,12 @@
  */
 
 // Comenzamos a trabajar ahora
+
+float calcularPromedio (float ventas[], int numDias){
+    float suma = 0.0;
+    for (int i = 0; i < numDias; i++){
+        suma += ventas[i];
+    }
+    float promedio = suma / numDias;
+    return promedio;
+}
